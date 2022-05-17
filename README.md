709bdc99ea17e531acc0e517cddc7d7521f6711e
Chlamydomonos <2457242458@qq.com>
2022-05-17 13:40:12 +0800

    Modified README.md and fixed color of rotten mycelial soil on grass blocks

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# FireAndRot （火焰与腐败）
+# ![](./src/main/resources/assets/f_a_r/textures/item/fire_and_rot_icon.png) FireAndRot （火焰与腐败）
 
-Teacon 2022的参赛作品
+[Teacon 2022](https://www.teacon.cn/2022/index) 的参赛作品
 
 Mod的主要玩法大概是创造了参照《艾尔登法环》中“猩红腐败”、其原型《风之谷》中“腐海”以及与之大概无关的《神秘时代4》中“腐化之地”的“腐败”，玩家需要发展各种与火焰有关的科技来消灭、控制、利用腐败。
--- src/main/java/xyz/chlamydomonos/f_a_r/loaders/BlockLoader.java
+++ src/main/java/xyz/chlamydomonos/f_a_r/loaders/BlockLoader.java
@@ -1,9 +1,12 @@
 package xyz.chlamydomonos.f_a_r.loaders;
 
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.GrassColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -29,5 +32,17 @@ public class BlockLoader
             ItemBlockRenderTypes.setRenderLayer(ROTTEN_MYCELIAL_SOIL.get(), RenderType.translucent());
             ItemBlockRenderTypes.setRenderLayer(ROTTEN_MYCELIAL_SOIL_HIDDEN.get(), RenderType.translucent());
         });
+    }
+
+    @SubscribeEvent
+    public static void onColorHandler(ColorHandlerEvent.Block event)
+    {
+        event.getBlockColors().register(
+                (a, b, c, d) -> b != null && c != null ?
+                                BiomeColors.getAverageGrassColor(b, c) :
+                                GrassColor.get(0.5D, 1.0D),
+                ROTTEN_MYCELIAL_SOIL.get(),
+                ROTTEN_MYCELIAL_SOIL_HIDDEN.get()
+                                       );
     }
 }
